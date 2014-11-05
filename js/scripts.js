5c38b0cc78ca393de70c62efdb33f90997d9abe8
UNA A. KRAVETS <uakravet@us.ibm.com>
2014-11-05 00:29:33 -0600

    Update 2014-11-05T06:29:33.285Z

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,15 @@ window.BLOG || (BLOG = {});
     $('.menu-section ul').toggleClass('hidden');
   });
 
+  //hljs
+  $(document).ready(function() {
+    $('pre code, code').each(function(i, e) {hljs.highlightBlock(e)});
+
+    //every time you click on the twitter link button, it takes the text inside of the blockquote and tweets :)
+    $('.twitter-share').each(function() {
+      $(this).attr('href', "https://twitter.com/share?url=http://www.google.com&via=Una&text=" + $(this).text());
+    });
+  });
+
   BLOG.common.init();
 })(jQuery, window, document);
-
-
