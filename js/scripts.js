0be1eaacffddc10d31e4446f67745f5f121f4916
UNA A. KRAVETS <uakravet@us.ibm.com>
2016-03-08 12:53:55 -0800

    jekyll search base files included

--- js/scripts.js
+++ js/scripts.js
@@ -35,6 +35,16 @@ window.BLOG || (BLOG = {});
     });
   });
 
+  // search
+  SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: 'search.json',
+    searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
+    noResultsText: 'No results found',
+    limit: 10
+  });
+
   // emoji
   function addEmoji() {
     if (navigator.userAgent.indexOf('Mac OS X') != -1) {
